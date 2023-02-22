59a3ea7a766e626e27e7c40344e9d63547fe2bb4
Sung Jun Cho <105574488+kimchosungjun@users.noreply.github.com>
2023-02-22 22:22:15 +0900

    [Silver V] Title: 나이순 정렬, Time: 44 ms, Memory: 7776 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct person
+{
+    int age;
+    string name; //int로 바꿔주면 오류 안남
+};
+
+bool compare(const person& now, const person& last)
+{
+    if(now.age != last.age) return now.age < last.age;
+    return false;
+}
+
+int main()
+{	
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    person people[100001]; //구조체 배열
+
+    for (int i = 0; i < N; i++) //배열에 값 넣어줌
+        cin >> people[i].age >> people[i].name;
+
+    //정렬
+    stable_sort(people, people + N, compare);
+
+    //답 출력
+    for (int i = 0; i < N; i++)
+        cout << people[i].age << " " << people[i].name << "\n";
+}
