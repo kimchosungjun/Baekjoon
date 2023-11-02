d4f242b1567cbde54f5c58c7e2c08c103b43181e
Sung Jun Cho <105574488+kimchosungjun@users.noreply.github.com>
2023-11-02 22:50:15 +0900

    [level unrated] Title: a와 b 출력하기, Time: 2.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#define setting ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+
+int main() {
+	setting;
+	int a, b;
+	cin >> a >> b;
+	cout << "a = " << a<<endl;
+	cout << "b = " << b<<endl;
+	return 0;
+}
