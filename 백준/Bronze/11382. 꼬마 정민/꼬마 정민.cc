636a7b6416ccecdc13929a00c8a8e80ad9f46fa8
Sung Jun Cho <105574488+kimchosungjun@users.noreply.github.com>
2024-05-29 23:32:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#define setting ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define llt long long int
+using namespace std;
+
+// 1577번
+
+#pragma region Variable
+llt a, b, c;
+#pragma endregion
+
+void Input() {
+	cin >> a >> b >> c;
+	cout << a + b + c;
+}
+
+void Solve() {
+	
+}
+
+int main() {
+	setting;
+	Input();
+	//Solve();
+	return 0;
+}
