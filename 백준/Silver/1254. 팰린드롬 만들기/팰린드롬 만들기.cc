e47d1cc7dcc43f9d9ab717d7ba9327a1a99a0c2e
Sung Jun Cho <105574488+kimchosungjun@users.noreply.github.com>
2024-06-01 23:56:51 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ 
+string s;
+int answer = 0;
+ 
+bool isPalindrom(string str)
+{
+    int cnt = str.size()/2;
+    for(int i=0; i<cnt; i++){
+        if(str[i]!=str[str.size()-1-i]){
+            return false;
+        }
+    }
+ 
+    return true;
+}
+ 
+int main()
+{
+    cin>>s;
+ 
+    answer = s.size()*2-1;
+    for(int i=0; i<s.size(); i++){
+        string temp = s.substr(i, s.size()-i);
+        if(isPalindrom(temp)){
+            answer = s.size()+i;
+            break;
+        }
+    }
+ 
+    cout<<answer<<"\n";
+    return 0;
+}
