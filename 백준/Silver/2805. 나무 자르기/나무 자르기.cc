16630b3d34bf2d1dc06ddab57869ab43eb598065
Sung Jun Cho <105574488+kimchosungjun@users.noreply.github.com>
2024-11-02 12:27:32 +0900

    [Silver II] Title: 나무 자르기, Time: 172 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,43 +1,57 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include<vector>
+#include<algorithm>
+#include<queue>
+#define Max 100001
 typedef long long ll;
+#define setting ios::sync_with_stdio(NULL), cin.tie(NULL)
+using namespace std;
 
-int main(int argc, const char* argv[]) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n;
-	ll m,num,result=0;
-	vector<ll>v;
-	cin >> n>>m;
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		v.push_back(num);
-	}
-	sort(v.begin(), v.end());
-	ll left = 0;
-	ll right = v[n-1];
-	while (left<=right) {
-		ll sum = 0,ans=0;
-		ll mid = (left + right) / 2;
-		for (int i = 0; i < n; i++) {
-			if (v[i] - mid > 0) {
-				sum += v[i] - mid;
-			}
-		}
-		if (sum >= m) {
-			left = mid + 1;
-			if (mid > result) {
-				result = mid;
-			}
-		}
-		else {
-			right = mid - 1;
-		}
-	}
-	cout << result;
-	return 0;
+vector<int>v;
+
+int main()
+{
+    setting;
+    int n;
+    ll m, num;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    ll left = 0;
+    ll right = v[n- 1];
+
+    ll mid = 0;
+    ll result = 0;
+
+    ll tree = 0;
+    while (left <= right) 
+    {
+        tree = 0;
+        mid = (left + right) / 2;
+        for (int i = 0; i < n; i++) {
+            if (v[i] - mid > 0)
+            {
+                tree += v[i] - mid;
+            }
+        }
+
+        if (m <= tree) 
+        {
+            left = mid + 1;
+            if(mid>result)
+                result = mid; 
+        }
+        else 
+        {
+            right = mid - 1;
+        }
+    }
+
+    cout << result;
+    return 0;
 }
